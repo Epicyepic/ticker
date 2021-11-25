9d884aa5889b89c6f27a08c6310d7b5b9f3ffa83
Epicyepic <94564079+Epicyepic@users.noreply.github.com>
2021-11-25 19:10:29 +0100

    Add files via upload

---
+++ Crycker-1.0.1/Crycker/Helper/TaskbarIconHelper.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Drawing;
+using System.Globalization;
+using System.Windows.Forms;
+
+namespace Crycker.Helper
+{
+    static class TaskbarIconHelper
+    {
+        public static NotifyIcon notifyIcon;
+
+        public static bool DarkMode { get; set; }
+        public static bool Highlight { get; set; }
+
+        private static string fontName;
+
+        static TaskbarIconHelper()
+        {
+            var settings = Settings.UserSettings.Load();
+            fontName = settings.FontName;
+        }
+
+        public static void SetPrice(decimal lastPrice, decimal previousPrice, string coin, string currency, string provider, DateTime lastUpdated)
+        {
+            var percentChange = 0.0M;
+            var brush = new SolidBrush(DarkMode ? Color.White : Color.Black);
+
+            if (lastPrice > 0 && Highlight)
+            {
+                percentChange = (lastPrice - previousPrice) / lastPrice * 100;
+                Logger.Info($"Change since last: {percentChange:N2}%");
+                if (percentChange != 0)
+                {
+                    Color priceColor = (percentChange >= 0) ? Color.Green : Color.OrangeRed;
+                    brush = new SolidBrush(priceColor);
+                }
+            }
+
+            var bitmap = new Bitmap(16, 16);
+            var graphics = Graphics.FromImage(bitmap);
+            var font = new Font(fontName, 7);
+
+            if (String.Compare(fontName, font.Name, StringComparison.InvariantCultureIgnoreCase) != 0)
+            {
+                Logger.Warning($"Font '{fontName}' not found, '{font.Name}' selected. Backing down to 'Tahoma'.");
+                font = new Font("Tahoma", 7);
+            }
+
+            // graphics.FillRectangle(Brushes.White, new Rectangle(0, 0, 16, 16));
+
+            string line1;
+            string line2;
+
+            if (lastPrice == 0)
+            {
+                line1 = "?";
+                line2 = "";
+            } else if (lastPrice < 1)
+            {                
+                var s = lastPrice.ToString("F3").Split(CultureInfo.CurrentCulture.NumberFormat.NumberDecimalSeparator[0]);
+                line1 = s[0];
+                line2 = s[1];
+            } else if (lastPrice > 1000)
+            {
+                var s = lastPrice.ToString("F0");
+                if (s.Length > 5) s = s.Substring(0, 6);
+                line1 = s.Substring(0, s.Length / 2);
+                line2 = s.Substring(s.Length / 2);
+            }
+            else
+            {
+                var s = lastPrice.ToString("F2").Split(CultureInfo.CurrentCulture.NumberFormat.NumberDecimalSeparator[0]);
+                line1 = s[0];
+                line2 = s[1];
+            }
+
+            var m = graphics.MeasureString(line1, font);
+            var w2 = (int)Math.Round(m.Width / 2);
+            var h2 = (int)Math.Round(m.Height / 2);
+
+            graphics.DrawString(line1, font, brush, bitmap.Size.Width / 2 - w2, -4 + bitmap.Size.Height / 2 - h2);
+
+            m = graphics.MeasureString(line2, font);
+            w2 = (int)Math.Round(m.Width / 2);
+            h2 = (int)Math.Round(m.Height / 2);
+            graphics.DrawString(line2, font, brush, bitmap.Size.Width / 2 - w2, 5 + bitmap.Size.Height / 2 - h2);
+
+            var hIcon = bitmap.GetHicon();
+            var icon = Icon.FromHandle(hIcon);            
+            notifyIcon.Icon = icon;
+
+            notifyIcon.Text = $"{provider} {coin}/{currency}: {lastPrice} ({percentChange:N2}%) @ {lastUpdated.ToLongTimeString()}";
+        }        
+    }
+}
---
+++ Crycker-1.0.1/README.md
@@ -0,0 +1,48 @@
+# CRYCKER
+
+Hey, you people that are contantly checking Bitcoin and other crypto currency prices on mobile or PC during meetings. You think we don't see you? We do!
+
+Here is a gift for you!
+
+> Crycker is a super simple tool that puts crypto currency price ticker into Windows tray.
+
+![Crycker in Windows 10 Taskbar](https://vidmar.net/img/projects/crycker-taskbar.gif)
+
+Nothing to install, no dependancies. Just a single file you put in your tools folder and run when needed. Configure to automatically start 
+with Windows. Right click to select source, currency and coin.
+
+<!-- ![Crycker](https://raw.githubusercontent.com/davidvidmar/Crycker/Cryker.png#floatright) -->
+
+Currently shows data from Bitstamp, Coinbase or Blockhain. Supports Bitcoin, Ethereum, Ripple, Lite Coin, with EUR and USD as currencies.
+
+![Crycker Settings](https://vidmar.net/img/projects/crycker-menu.png)
+
+**Download**    - [Version 1.0.1](https://github.com/davidvidmar/Crycker/releases/latest)
+
+**Home Page** - [Source Code](https://vidmar.net/Cryker/)
+
+Have an idea?  Found a bug? [Leave feedback](mailto:crycker@vidmar.net) or [submit an issue](https://github.com/davidvidmar/Crycker/issues)!
+
+---
+
+## Change log: ##
+
+**v1.0.1** - 7th Oct 2017 
+
+* check for updates automatically, can be turned off in config file
+* support for generic currencies, that can be set in config file
+* read config file from directory where executable is, not from current working directory
+* Bitstamp sometimes returns bogus answer, ignore it
+* more robust logging, that could crash the app under certain conditions
+
+---
+
+If you like Crycker, please donate! 
+
+**ETH** to "0x16c99026fc3251fa03e0372c496403ec4ffbd80d" or use code:
+
+![ETH donation address](https://vidmar.net/img/projects/donate-eth.png)
+
+**BTC** to "35aF5bLUa28AYEW22tVsRYbgEkBdyc9wzf" or use QR code:
+
+![BTC donation address](https://vidmar.net/img/projects/donate-btc.png)
